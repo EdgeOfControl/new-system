604335ac76ddca655efae56cea63b6df65e83534
EdgeOfControl <55520355+EdgeOfControl@users.noreply.github.com>
2020-07-30 23:36:42 -0400

    Added auxiliary batch file
    Replaces Notepad with Notepad++ when the notepad run command is initialized. The user is only prompted to run this batch if they agree to install the developer software suite under the function developer.

---
+++ notepadreplace.bat
@@ -0,0 +1,6 @@
+@echo off
+@echo Replacing Notepad with Notepad++
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles(x86)%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f
+@echo Notepad++ replaced Notepad as the notepad run command.
+pause
+exit
